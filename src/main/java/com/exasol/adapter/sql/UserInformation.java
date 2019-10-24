9b135a0b830a56df0ab67281e313dbc6369bf72b
Anastasiia Sergienko <anastasiia.sergienko@exasol.com>
2019-10-24 15:36:20 +0200

    #2: added UserInformation tests

--- src/main/java/com/exasol/adapter/sql/UserInformation.java
+++ src/main/java/com/exasol/adapter/sql/UserInformation.java
@@ -8,10 +8,8 @@ public class UserInformation {
     public int getRoleMask(final Connection connection) {
         final String rlsUsersTableName = "rls_users";
         final String query = "SELECT exa_role_mask FROM " + rlsUsersTableName + " WHERE exa_user_name = CURRENT_USER";
-        try (final PreparedStatement statement = connection.prepareStatement(query)) {
-            final ResultSet resultSet = statement.executeQuery();
-            final int exaRoleMask = resultSet.getInt("exa_role_mask");
-            return exaRoleMask;
+        try (final ResultSet resultSet = connection.prepareStatement(query).executeQuery()) {
+            return resultSet.getInt("exa_role_mask");
         } catch (final SQLException exception) {
             throw new RemoteMetadataReaderException(
                   "Unable to read role mask from " + rlsUsersTableName + ". Caused by: " + exception.getMessage(),
